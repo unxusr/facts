d2f4762dd5a79896a6bac99d168a5941cfb03f14
Moamen Sayed <moamen@ymail.com>
2022-06-23 14:16:40 +0200

    enhance the return text + remove commented experiments

--- named_entity_recognition.py
+++ named_entity_recognition.py
@@ -11,17 +11,4 @@ def get_dates_cardinals(sent):
             cardinals.append(ent)
         if ent.label_ == "DATE":
             dates.append(ent)
-    return f"Found dates: {dates}", f"Found Cardinals: {cardinals}"
-
-
-
-# Salah PERSON
-# 2 CARDINAL
-# 2020 DATE
-# Google ORG
-
-sent = "Salah went to Maldives 2 times in June 2020 and Google 3rd of August recorded that "
-#sent = "Apple is looking at buying U.K. startup for $1 billion"
-#sent = sys.argv[1]
-#sys.stdin[1]
-get_dates_cardinals(sent)
+    return f"dates: {dates}", f"Cardinals: {cardinals}"
